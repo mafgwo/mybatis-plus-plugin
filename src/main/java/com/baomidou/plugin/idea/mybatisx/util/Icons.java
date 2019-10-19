c3c8daa2e84e3aaf5c0fbf15a7f802acfaa056bf
kana112233 <makey22112@gmail.com>
2019-10-19 16:05:13 +0800

    Fix xml mapper png (#11)
    * fix xml mapper svg error
    
    * fix chang log

--- src/main/java/com/baomidou/plugin/idea/mybatisx/util/Icons.java
+++ src/main/java/com/baomidou/plugin/idea/mybatisx/util/Icons.java
@@ -9,11 +9,12 @@ import javax.swing.*;
  * 图标
  */
 public interface Icons {
+
     Icon MYBATIS_LOGO = IconLoader.getIcon("/images/logo.png");
 
     Icon PARAM_COMPLETION_ICON = PlatformIcons.PARAMETER_ICON;
 
-    Icon MAPPER_LINE_MARKER_ICON = IconLoader.getIcon("/images/xml-mapper.svg");
+    Icon MAPPER_LINE_MARKER_ICON = IconLoader.getIcon("/images/xml-mapper.png");
 
     Icon STATEMENT_LINE_MARKER_ICON = IconLoader.getIcon("/images/java.png");
 
